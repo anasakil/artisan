bb2f2fc1b018eb6b084f74abbcb0d32a6747c02f
badr <dridakhbadr@gmail.com>
2024-03-21 23:32:39 +0100

    logged in users orders and get user by Id

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -45,3 +45,19 @@ const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json(createdOrder);
   }
 });
+
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
