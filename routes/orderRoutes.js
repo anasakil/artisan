5f3c60b51246c972a439a9fdd1d0bc7ed2a71001
badr <dridakhbadr@gmail.com>
2024-03-21 23:40:58 +0100

    add order Routes

---
+++ routes/orderRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+} = require('../controllers/orderController');
+const { protect, admin } = require('../middleware/authMiddleware');
+
+//  routes
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
+router.route('/mine').get(protect, getMyOrders);
+router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
+
+module.exports = router;
+
