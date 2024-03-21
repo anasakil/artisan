cf6f5486a7b93a0fb4ccda2661b6145f42d450bb
badr <dridakhbadr@gmail.com>
2024-03-21 23:27:03 +0100

    Create new order and calculte prices

---
+++ controllers/orderController.js
@@ -0,0 +1,47 @@
+const asyncHandler = require('../middleware/asyncHandler');
+const Order = require('../models/Order');
+const Product = require('../models/Product');
+const { calcPrices } = require('../utils/calcPrices');
+const { verifyPayPalPayment, checkIfNewTransaction } = require('../utils/paypal');
+
+const addOrderItems = asyncHandler(async (req, res) => {
+  const { orderItems, shippingAddress, paymentMethod } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order items');
+  } else {
+    const itemsFromDB = await Product.find({
+      _id: { $in: orderItems.map((x) => x._id) },
+    });
+
+    const dbOrderItems = orderItems.map((itemFromClient) => {
+      const matchingItemFromDB = itemsFromDB.find(
+        (itemFromDB) => itemFromDB._id.toString() === itemFromClient._id
+      );
+      return {
+        ...itemFromClient,
+        product: itemFromClient._id,
+        price: matchingItemFromDB.price,
+        _id: undefined,
+      };
+    });
+
+    const { itemsPrice, taxPrice, shippingPrice, totalPrice } = calcPrices(dbOrderItems);
+
+    const order = new Order({
+      orderItems: dbOrderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
