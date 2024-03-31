ad3651a379687d3c6a2bd1e2603edf552e26be17
anasakil <akil87382@gmail.com>
2024-03-31 18:10:40 +0000

    Fix package vulnerabilities1

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-require('dotenv').config();
 const express = require('express');
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDB = require('./config/db.js');
