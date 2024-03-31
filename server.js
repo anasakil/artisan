177aaf937b5f3700deed7feadf2f09c2f65a7090
anasakil <akil87382@gmail.com>
2024-03-31 18:13:58 +0000

    Fix package vulnerabilities2

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const express = require('express');
 require('dotenv').config();
-
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDB = require('./config/db.js');
