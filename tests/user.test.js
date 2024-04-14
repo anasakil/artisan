2bb450ef63acb01ec0a140501b0af5873465c053
anasakil <akil87382@gmail.com>
2024-04-14 21:07:41 +0100

    config test with jest2

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,27 +1,27 @@
+const mongoose = require('mongoose');
+const app = require('../server');
 const request = require('supertest');
-const app = require('../server'); 
-describe('User Endpoints', () => {
-    // it('should create a new user', async () => {
-    //     const res = await request(app)
-    //         .post('/api/users/register')
-    //         .send({
-    //             username: 'user',
-    //             email: 'usezzr@example.com',
-    //             password: '123456',
-    //             role: 'buyer'
-    //         });
-    //     expect(res.statusCode).toEqual(201);
-    //     expect(res.body).toHaveProperty('token');
-    // });
 
-    it('should authenticate a user', async () => {
-        const res = await request(app)
-            .post('/api/users/login')
-            .send({
-                email: 'user@example.com',
-                password: '123456'
-            });
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty('token');
-    });
+let server;
+
+beforeAll(() => {
+  server = app.listen(3002); // Start server
+});
+
+afterAll(async () => {
+  server.close(); // Close server
+  await mongoose.disconnect(); // Disconnect from the database
+});
+
+describe('User Endpoints', () => {
+  it('should authenticate a user', async () => {
+    const res = await request(server)
+      .post('/api/users/login')
+      .send({
+        email: 'user@example.com',
+        password: '123456'
+      });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('token');
+  });
 });
